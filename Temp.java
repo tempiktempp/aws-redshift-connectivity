f5aebc0584b0e81f02da203c56cf612f309f8dfc
tempiktempp <tempiktempp@gmail.com>
2026-03-11 15:17:32 +0530

    Update Temp.java

--- Temp.java
+++ Temp.java
@@ -1,16 +1,48 @@
-@Valid
-private ProxyProperties proxy = new ProxyProperties();
+@Bean
+@ConditionalOnMissingBean
+@ConditionalOnProperty(
+        prefix = "redshift",
+        name = "connection-strategy",
+        havingValue = "DATA_API",
+        matchIfMissing = true)
+public RedshiftDataClient redshiftDataClient(
+        RedshiftProperties props,
+        AwsCredentialsProvider credentialsProvider) {
 
-@Getter
-@Setter
-public static class ProxyProperties {
+    log.info("[Redshift] Initialising Data API client, " +
+            "region: {}", props.getRegion());
 
-    private boolean enabled = false;
-    private String host;
+    RedshiftDataClient.Builder builder =
+            RedshiftDataClient.builder()
+                    .region(Region.of(props.getRegion()))
+                    .credentialsProvider(credentialsProvider);
 
-    @Min(1) @Max(65535)
-    private int port = 8080;
+    // Apply proxy config if enabled
+    if (props.getProxy().isEnabled()) {
+        log.info("[Redshift] Proxy enabled: {}:{}",
+                props.getProxy().getHost(),
+                props.getProxy().getPort());
 
-    private String username;
-    private String password;
-}
+        ProxyConfiguration.Builder proxyBuilder =
+                ProxyConfiguration.builder()
+                        .endpoint(URI.create(
+                                "http://" +
+                                props.getProxy().getHost() +
+                                ":" +
+                                props.getProxy().getPort()));
+
+        // Only set credentials if proxy requires auth
+        if (props.getProxy().getUsername() != null
+                && !props.getProxy().getUsername().isBlank()) {
+            proxyBuilder
+                    .username(props.getProxy().getUsername())
+                    .password(props.getProxy().getPassword());
+        }
+
+        builder.httpClientBuilder(
+                ApacheHttpClient.builder()
+                        .proxyConfiguration(proxyBuilder.build()));
+    }
+
+    return builder.build();
+                            }
