945e444e42d80406a53d1417a767adbbef814dce
tempiktempp <tempiktempp@gmail.com>
2026-03-11 19:58:25 +0530

    Update README.java

--- README.java
+++ README.java
@@ -4,17 +4,31 @@ private ApacheHttpClient.Builder buildApacheHttpClient(
     RedshiftProperties.ProxyProperties proxy =
             props.getProxy();
 
+    log.info("[Proxy-Debug] host={}, port={}, user={}",
+            proxy.getHost(),
+            proxy.getPort(),
+            proxy.getUsername());
+
+    URI proxyEndpoint = URI.create(
+            "http://" +
+            proxy.getHost() +
+            ":" +
+            proxy.getPort());
+
     return ApacheHttpClient.builder()
+            .connectionTimeout(
+                    java.time.Duration.ofSeconds(30))
+            .socketTimeout(
+                    java.time.Duration.ofSeconds(60))
             .proxyConfiguration(
                 software.amazon.awssdk.http.apache
                     .ProxyConfiguration.builder()
-                        .endpoint(URI.create(
-                                "http://" +
-                                proxy.getHost() +
-                                ":" +
-                                proxy.getPort()))
+                        .endpoint(proxyEndpoint)
                         .username(proxy.getUsername())
                         .password(proxy.getPassword())
+                        .preemptiveBasicAuthenticationEnabled(
+                                Boolean.TRUE)
+                        .useSystemPropertyValues(Boolean.FALSE)
                         .addNonProxyHost("localhost")
                         .addNonProxyHost("127.0.0.1")
                         .build());
